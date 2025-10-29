6690e297e438c6cdf53946e0db918090eef1a7e3
sirajkhan776 <sirajkhan0886@gmail.com>
2025-10-29 19:44:20 +0000

    chore: re-enable Django admin at /admin/ alongside custom dashboard

--- reyhardy/urls.py
+++ reyhardy/urls.py
@@ -4,6 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path("admin/", admin.site.urls),
     path("", include("core.urls")),
     path("accounts/", include("accounts.urls")),
     path("accounts/", include("allauth.urls")),
