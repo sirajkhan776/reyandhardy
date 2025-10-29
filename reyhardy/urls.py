6f9370d8c366e1b32597d3d5f7fd4ca223e4aa61
sirajkhan776 <sirajkhan0886@gmail.com>
2025-10-29 18:44:25 +0000

    feat(admin): hide default Django admin route and add links to custom /dashboard/ for staff in desktop and mobile

--- reyhardy/urls.py
+++ reyhardy/urls.py
@@ -4,7 +4,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
     path("", include("core.urls")),
     path("accounts/", include("accounts.urls")),
     path("accounts/", include("allauth.urls")),
